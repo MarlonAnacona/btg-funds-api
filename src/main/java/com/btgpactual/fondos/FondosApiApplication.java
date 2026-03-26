d9339f74a01e30ef6cb1ef2004785200966a95f1
MarlonAnacona <marlon.andres.anacona@gmail.com>
2026-03-26 14:43:51 -0500

    first commit

---
+++ src/main/java/com/btgpactual/fondos/FondosApiApplication.java
@@ -0,0 +1,13 @@
+package com.btgpactual.fondos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FondosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btgpactual/fondos/FondosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btgpactual.fondos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
